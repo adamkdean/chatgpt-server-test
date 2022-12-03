6970d1fbfebccb8c3646add996c23e8f1731ac64
Adam K Dean <adamkdean@googlemail.com>
2022-12-03 00:08:46 +0000

    sessions working

--- src/index.js
+++ src/index.js
@@ -7,27 +7,68 @@ const WebSocket = require('ws')
 const port = process.env.PORT || 8080
 const server = new WebSocket.Server({ port })
 
+let sessionActive = false
+let connections = {}
+
 server.on('listening', () => {
   console.log(`Listening for connections on port ${port}`)
 })
 
 server.on('connection', (socket, req) => {
   const searchParams = new URLSearchParams(req.url.slice(1))
-  console.log(searchParams)
   const username = searchParams.get('username')
+
+  // Ensure username is provided
   if (!username) {
     console.log('No username provided', username)
     socket.terminate()
     return
   }
 
-  console.log('New connection from', username)
+  // Ensure two connections are connected at most
+  if (connections.length >= 2) {
+    console.log('Too many connections', connections.length)
+    socket.terminate()
+    return
+  }
+
+  // Ensure username is unique
+  if (connections[username]) {
+    console.log('Username already exists', username)
+    socket.terminate()
+    return
+  }
+
+  console.log('New connection from user:', username)
+  connections[username] = socket
+
+  // Send session active message to new connection
+  const otherUser = Object.keys(connections).find((user) => user !== username)
+  if (otherUser) {
+    console.log('Sending offer to', otherUser)
+    connections[otherUser].send('SESSION_ACTIVE')
+    socket.send('SESSION_ACTIVE')
+    sessionActive = true
+  } else {
+    socket.send('SESSION_PENDING')
+  }
 
   socket.on('message', (message) => {
     console.log(`Received message: ${message}`)
   })
 
   socket.on('close', () => {
-    console.log('Connection closed')
+    delete connections[username]
+    console.log(`User ${username} disconnected`)
+    if (sessionActive) {
+      // terminate all sockets
+      Object.keys(connections).forEach((user) => {
+        connections[user].send('SESSION_TERMINATED')
+        connections[user].terminate()
+      })
+      connections = {}
+      console.log('Session ended')
+      sessionActive = false
+    }
   })
 })
